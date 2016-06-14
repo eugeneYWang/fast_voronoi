2ee19d994ae481746d96d7b3cfab47884d4a7687
eugeneYWang <eugenewang@outlook.com>
2016-06-14 12:27:58 -0400

    comments added. organized.

--- TimorLesteAIMS_GeocodedResearchRelease_Level1_v1.4.1/Test_structuring_v0.1.py
+++ TimorLesteAIMS_GeocodedResearchRelease_Level1_v1.4.1/Test_structuring_v0.1.py
@@ -19,22 +19,11 @@ def combineTwoList(l1, l2):
                 l1.append(str)
                 continue
     return l1
-
+# --------------------------------------------------------
 
 # read csv | 1. Pandas used here
 address = os.getcwd()
 oldCSV = pd.read_csv(address+'//data//level_1a.csv')
-
-#row_number, col_number = oldCSV.shape # get count of rows and columns
-#comparing_record_index = 0
-
-
-# while len(dataframe)>0:
-# I do not need to get the list of indices to loop for anymore
-# find the first record, and get their lat and lon coordinate records.
-# find those records that share spatial position with the first record
-# use set() to find the unique iso code.
-# use dataframe = dataframe[~(lat&lon)] to find out those records still need to be checked
 
 # set up the list of column title
 listCol = ['latitude', 'longitude', 'donors_iso3']
@@ -47,6 +36,7 @@ comparing_index = 0
 count_total = len(oldCSV)
 
 while len(oldCSV) > 0:
+    # get the latitude and longitude to compare with others
     lat_comparing = oldCSV.head(1).latitude[comparing_index]
     lon_comparing = oldCSV.head(1).longitude[comparing_index]
     is_nan_lat = str(lat_comparing) == 'nan'
@@ -54,6 +44,9 @@ while len(oldCSV) > 0:
 
     is_lat = oldCSV.latitude == lat_comparing
     is_lon = oldCSV.longitude == lon_comparing
+
+    # if lat or lon is NaN, jump to next loop and continue comparing
+    # if the rest records are all NaN on lat and lon, break the loop
     if (is_nan_lat | is_nan_lon):
         oldCSV = oldCSV[is_lat | is_lon]
         if len(oldCSV) == 0:
@@ -62,12 +55,13 @@ while len(oldCSV) > 0:
         count_now = len(oldCSV)
         print "progress : %s / %s" % (count_now, count_total)
         continue
+
     # get dataframe of overlaping points
     subset_overlapping = oldCSV[is_lat & is_lon]
     set_ISO3 = set(subset_overlapping.donors_iso3.tolist())
     str_donor = ''
 
-    # to get a list of unique values of ISO3 code
+    # get a list of unique values of ISO3 code
     list_donor = []
     for e in set_ISO3:
         temp_list_donor = str(e).split('|')
@@ -81,22 +75,17 @@ while len(oldCSV) > 0:
         str_donor += strItem
         ii += 1
 
-
+    # a row of records on one position
     list_temp = {'latitude':lat_comparing,'longitude':lon_comparing,'donors_iso3':str_donor}
     list_rows.append(list_temp)
-    # get the rest of records not checked yet
+    # next loop will check the rest of records not checked yet
     oldCSV = oldCSV[~(is_lat & is_lon)]
     # have a new index to compare with the rest records.
     comparing_index = oldCSV.head(1).index[0]
     count_now = len(oldCSV)
     print "progress : %s / %s" %(count_now, count_total)
 
+# create the dataframe with all rows of information about positions
 cleaned_data = pd.DataFrame(list_rows)
 cleaned_data.to_csv('cleaned_Donors_record.csv', encoding='utf-8')
 
-
-
-    # create a dataframe
-
-    # create a row to append
-    # find a way to append a row to a dataframe
